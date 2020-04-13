3f47d792c8dabe694972b3ebcc070b3da363603f
manu <manuhortet@gmail.com>
2020-04-13 14:14:29 +0200

    Bot sends picture when receiving simple URL with no commands

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,6 @@
 import telegram
 import logging
-from telegram.ext import CommandHandler, Updater
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 from telegram.ext.dispatcher import run_async
 
 from bot.downloader import download_picture
@@ -22,39 +22,40 @@ def start(bot, update):
 @run_async
 def download(bot, update):
 
-    if len(update.message.entities) != 2:
+    if len(update.message.entities) != 1:
         bot.sendMessage(chat_id=update.message.chat_id,
-                        text="Invalid format, I need an URL to download from. Try /help.")
+                        text="Invalid format. Try /help.")
         logging.warning(
             f'{update.effective_user.name} tried to download without providing an URL.')
         return
 
-    link = update.message.parse_entity(update.message.entities[1])
+    link = update.message.parse_entity(update.message.entities[0])
     picture, author = download_picture(link)
 
     if picture:
-        bot.sendPhoto(chat_id=update.message.chat_id, photo=picture, caption=f'via @{author}')
-        logging.info(f'Sending {picture} to {update.effective_user.name}.')
+        bot.sendPhoto(chat_id=update.message.chat_id, photo=picture,
+                      caption=f"via @{author.replace(' ', '-')}")
+        logging.info(f'Sending {link} to {update.effective_user.name}.')
     else:
         bot.sendMessage(chat_id=update.message.chat_id,
                         text="I couldn't find any picture following that link.")
-        logging.warning(f'Failed to send {picture} to {update.effective_user.name}.')
+        logging.warning(f'Failed to send {link} to {update.effective_user.name}.')
 
 
 @run_async
 def help(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id,
-                    text="To download a picture from VSCO, use /download directly followed by the picture public URL. Something like:\n\n"
-                         "/download https://vsco.co/user/media/1")
+                    text="To download a picture from VSCO, just share the public URL of the picture with me.")
 
 
 def main():
     updater = Updater(token=token)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler('download', download))
     dispatcher.add_handler(CommandHandler('help', help))
     dispatcher.add_handler(CommandHandler('start', start))
+
+    dispatcher.add_handler(MessageHandler(Filters.text, download))
 
     logging.info("Bot running at @VSCOdownloader_bot")
     updater.start_polling()
--- bot/downloader.py
+++ bot/downloader.py
@@ -9,7 +9,7 @@ def download_picture(url):
     if not is_valid_url(url):
         import pdb
         pdb.set_trace()
-        return None
+        return None, None
 
     if is_mobile_url(url):
         url = get_public_url(url)
@@ -17,10 +17,10 @@ def download_picture(url):
     response = requests.get(url)
     if response.status_code is 404:
         pdb.set_trace()
-        return None
+        return None, None
 
-    id = url.rsplit('/')[5]
-    return get_picture_and_author(response.text, id)
+    picture_id = url.rsplit('/', 1)[-1]
+    return get_picture_and_author(response.text, picture_id)
 
 
 def is_mobile_url(url):
@@ -45,12 +45,12 @@ def get_public_url(url):
     author = raw_link.rsplit('/')[3]
     id = raw_link.rsplit('/')[5]
 
-    return f'https://vsco.co/{author}/media/{id}/'
+    return f'https://vsco.co/{author}/media/{id}'
 
 
-def get_picture_and_author(html, id):
+def get_picture_and_author(html, pic_id):
     preload_state = re.findall('window.__PRELOADED_STATE__ = (.*?)</script>', html)[0]
-    picture_data = json.loads(preload_state).get("medias").get("byId").get(id).get('media')
+    picture_data = json.loads(preload_state).get("medias").get("byId").get(pic_id).get('media')
 
     author = picture_data.get('gridName')
     file_url = "https://" + picture_data.get('responsiveUrl')
